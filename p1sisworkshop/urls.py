7614c45316bd97f38e97506fd02baa5bc64fed67
SebasL07 <sebasgnv0207@gmail.com>
2023-10-30 09:13:45 -0500

    Add more stuff

--- p1sisworkshop/urls.py
+++ p1sisworkshop/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from courses.views.professor_create import ProfessorCreate
@@ -32,3 +34,7 @@ urlpatterns = [
     path('signin/', Signin.as_view(), name="signin"),
     path('signout/', CoursesMain.signout, name="signout"),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
